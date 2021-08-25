348b6971c178b8401560e73dd0c8af39d7c4fc63
Daniel Portillo <depr.daniel@gmail.com>
2021-08-25 13:27:28 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const logo = $(".logo-header");
+
+$(window).scroll(function(){
+    $("header").toggleClass("sticky", $(window).scrollTop() > 0.1);
+    if ($(window).scrollTop() > 0.1) {
+        logo.attr("src", "images/Logos/logo.png");
+    } else {
+        logo.attr("src", "images/Logos/logo-acuarela.png")
+    }
+    
+})
